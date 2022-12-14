d7cb85a4150074621bc81544ff3e2afee1006828
rkdwlss2 <rkdwlss2@gmail.com>
2022-12-14 23:50:40 +0900

    챗팅 룸으로 구현 ㅎ

--- src/main/java/com/cos/chatapp/ChatRepository.java
+++ src/main/java/com/cos/chatapp/ChatRepository.java
@@ -9,4 +9,8 @@ public interface ChatRepository extends ReactiveMongoRepository<Chat,String> {
     @Tailable
     @Query("{sender:?0,receiver:?1}")
     Flux<Chat> mFindBySender(String sender, String receiver); // flux(흐름을 ) response를 유지하면서 데이터를 계속 흘려 보내기
+
+    @Tailable
+    @Query("{roomNum:?0}")
+    Flux<Chat> mFindByRoomNum(Integer roomNum); // flux(흐름을 ) response를 유지하면서 데이터를 계속 흘려 보내기
 }
